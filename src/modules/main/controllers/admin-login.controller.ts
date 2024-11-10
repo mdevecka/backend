d56ae38d5d465f584aed78f6d5a294b01dcf2fae
Marián Devečka <marian.devecka@gmail.com>
2024-11-10 17:17:32 +0100

    fixes

--- src/modules/main/controllers/admin-login.controller.ts
+++ src/modules/main/controllers/admin-login.controller.ts
@@ -1,22 +1,20 @@
 import { Controller, Post, Response, UseGuards, Body } from '@nestjs/common';
 import { Response as ExpressResponse } from 'express';
 import { AuthService } from '@modules/auth/services';
-import { AuthGuard, SESSION_COOKIE, GetSessionId, Public } from '@modules/auth/helpers';
+import { SessionAuthGuard, SESSION_COOKIE, GetSessionId, Public } from '@modules/auth/helpers';
 import { LoginDto } from '../contracts/admin/login';
 
-@UseGuards(AuthGuard)
+@UseGuards(SessionAuthGuard)
 @Controller('admin')
 export class AdminLoginController {
 
   constructor(private authService: AuthService) {
   }
 
-  @Public()
   @Post('login')
   async login(@Body() login: LoginDto, @Response({ passthrough: true }) res: ExpressResponse) {
-    const sessionId = await this.authService.login(login.email, login.password);
+    const sessionId = await this.authService.loginWithCredentials(login.email, login.password);
     res.cookie(SESSION_COOKIE, sessionId, { httpOnly: true, secure: true, sameSite: "strict" });
-    return { sessionId };
   }
 
   @Post('logout')
